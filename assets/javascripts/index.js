dfe6c3c90182953d20a756bf9e6aa63c3c20751f
Gabriel Lima <gabriel.wprog@gmail.com>
2018-07-21 00:50:13 -0300

    WIP: create tabs from api response

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,15 +1,13 @@
-import { Tabs } from 'materialize/js/materialize.js';
+import M from 'materialize/js/materialize.js';
 
 const app = document.getElementById('app');
 
-const CONTENT_COL_CLASSES = ['col', 's12']
+const CONTENT_COL_CLASSES = ['col', 's12', 'm12']
 
 const createTable = (caption, rows) => {
-  window.rows = rows
-  console.log(rows)
   return `<table>
+            <caption>${caption}</caption>
             <thead>
-              <caption>${caption}</caption>
               <tr>
                 <th>Posição</th>
                 <th>Resultado</th>
@@ -26,27 +24,29 @@ const makeRows = ({position, result, animal}) => {
   return `<tr><td>${position}</td><td>${result}</td><td>${animal}</td></tr>`
 }
 
-const createTableLinks = (games) => {
-  return Array.from(games).map((game) => {
-    return `<li class='tab col s3'>
-              <a href='#${game.type}'>${name.type}</a>
+const createTableLinks = (results) => {
+  return Object.keys(results).map((game_type) => {
+    return `<li class='tab col m3 s3'>
+              <a href='#${game_type}'>${game_type}</a>
             </li>`
   })
 }
 
+const createElement = (tagname) => document.createElement(tagname)
+
 const createTabsContent = (caption, results) => {
-  let games = Object.keys(results)
+  let game_types = Object.keys(results)
   var tabContainer = document.getElementById('tabs')
 
-  games.forEach((game) => {
-    let div = document.createElement('div')
-    div.classList.add(CONTENT_COL_CLASSES)
-    div.id = `#${game}`
+  game_types.forEach((game_type) => {
+    let div = createElement('div')
+    div.classList.add(...CONTENT_COL_CLASSES)
+    div.id = game_type
 
-    let rows = Array.from(results[game])
+    let rows = Array.from(results[game_type])
       .map((gameResult) => makeRows(gameResult))
 
-    div.innerHTML = createElement(caption, rows)
+    div.innerHTML = createTable(caption, rows)
 
     tabContainer.appendChild(div)
   })
@@ -74,8 +74,10 @@ const getGameResults = () => {
 getGameResults()
   .then((response) => {
     var { caption, results } = JSON.parse(response)
-    var links = createTableLinks(results)
+    var links = createTableLinks(results).join(' ')
+    var ul = document.getElementById('tab-navigation')
+    ul.innerHTML = links
     var tabs = createTabsContent(caption, results)
-    document.getElementById('tab-navigation').innerHTML = links
+    M.Tabs.init(ul, { swipeable: true }, 1)
   }).catch((error) => console.log(error))
 
