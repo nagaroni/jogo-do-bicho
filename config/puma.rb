2c0592c4e7b1d5c44abb0ede4965345eca907664
Gabriel Lima <gabriel.wprog@gmail.com>
2019-04-08 19:04:44 -0300

    Update Gemfile and change docker-compose.yml

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,5 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RACK_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-ENV['SITE_URL'] ||= 'http://www.ojogodobicho.com/deu_no_poste.htm'
-
 port        ENV['PORT']     || 3000
-
 environment ENV['RACK_ENV'] || 'development'
